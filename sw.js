6e1de9c9867179a0146cbbb0b355cf27645a92f7
mugol.portal <mugol.education.help@gmail.com>
2026-08-11 09:53:15 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+// MuGöl Hikayeler - basit service worker (PWA kurulabilirliği için)
+const CACHE_NAME = 'mugol-hikayeler-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).catch(() => {})
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).catch(() => cached);
+    })
+  );
+});
